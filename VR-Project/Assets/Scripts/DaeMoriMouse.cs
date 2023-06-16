6c4fb23453ec7c352d7523a8fed491ee34d261dc
kangtp <115209262+kangtp@users.noreply.github.com>
2023-06-17 02:54:10 +0900

    2:54 food check

--- VR-Project/Assets/Scripts/DaeMoriMouse.cs
+++ VR-Project/Assets/Scripts/DaeMoriMouse.cs
@@ -5,20 +5,49 @@ using UnityEngine;
 public class DaeMoriMouse : MonoBehaviour
 {
     public GameObject daeMori;
-    public AudioSource eatSound;
+    public AudioSource audioSource;
+    public AudioClip goodEat;
+    public AudioClip badEat;
+    private string[] damoriSkewer = { "Leg", "Salmon", "Salmon", "Chop" };
+    private string[] makeSkewer;
 
     private void Start()
     {
-
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("food"))
+        if (other.CompareTag("Stick"))
         {
-            eatSound.Play();
-            daeMori.GetComponent<Animator>().SetInteger("Condition", 1);
-            Debug.Log("daeMori eat");
+            makeSkewer = FindObjectOfType<RecognizeFood>().GetSkewerFoods();
+            if (CheckSkewer(makeSkewer) == true)
+            {
+                audioSource.clip = goodEat;
+                audioSource.Play();
+                daeMori.GetComponent<Animator>().SetInteger("Condition", 1);    //happy
+                Debug.Log("음 좋아요");
+            }
+            else
+            {
+                audioSource.clip = badEat;
+                audioSource.Play();
+                daeMori.GetComponent<Animator>().SetInteger("Condition", 2);    //angry
+                Debug.Log("이거 아니잖아");
+            }
+            
         }
+    }
+
+    bool CheckSkewer(string[] mySkewer)
+    {
+        for(int i = 0; i < makeSkewer.Length; i++)
+        {
+            if (mySkewer[i] != damoriSkewer[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
