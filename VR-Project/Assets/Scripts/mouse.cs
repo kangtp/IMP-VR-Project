06b23656c2c3f1e97adf16d2b3d048834939f73d
ykd0638 <57110958+ykd-yang@users.noreply.github.com>
2023-06-17 04:36:52 +0900

    6.17 4:36

--- VR-Project/Assets/Scripts/mouse.cs
+++ VR-Project/Assets/Scripts/mouse.cs
@@ -2,24 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mouse : MonoBehaviour
+public class Mouse : MonoBehaviour
 {
-    public GameObject bigboy;
-    public AudioSource eatSound;
+    public GameObject original;
 
-    private void Start()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("food"))
+        if (other.CompareTag("Stick"))
         {
-            eatSound.Play();
-            bigboy.GetComponent<Animator>().SetInteger("Condition", 1);
-            Debug.Log("bigboy eat");
+            original.GetComponent<EatModeScript>().CheckSkewer();
         }
-
     }
 }
