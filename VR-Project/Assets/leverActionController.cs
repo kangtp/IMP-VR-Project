152bf28d92fec41f65bb2c65c7dacdfeb074aa36
kangtp <115209262+kangtp@users.noreply.github.com>
2023-06-17 15:40:04 +0900

    3:40

---
+++ VR-Project/Assets/leverActionController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leverActionController : MonoBehaviour
+{
+
+    public void MaxLimit()
+    {
+        Debug.Log("Max reached!");
+    }
+
+    public void MinLimit()
+    {
+        Debug.Log("Min reached!");
+    }
+
+    public void Zero()
+    {
+        Debug.Log("Zero reached!");
+    }
+
+
+}
